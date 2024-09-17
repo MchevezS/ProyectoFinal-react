b85724a00d45b0637f3d81775479c604e9ad5c78
MchevezS <168207997+MchevezS@users.noreply.github.com>
2024-09-17 16:12:54 -0600

    commit header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,4 @@
-
+import '../style/Header.css'
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
@@ -12,31 +12,44 @@ function BasicExample() {
   <>
   <div className='contenedorRedes'>
     <div className='redesSociales'>
-      <Link to={"Facebook"}> <i className="fa-brands fa-facebook"></i> </Link>
-        <Link to={"Whatsapp"}> <i className="fa-brands fa-whatsapp"></i> </Link>
-          <Link to={"Insta"}> <i className="fa-brands fa-instagram"></i> </Link> 
-           <Link to={"Ubicacion"}> <i className="fa-solid fa-location-dot"></i> </Link>
-     </div>
-  <div className='correoTelefono'>
+      <div className='face'>
+      <Link to={"Facebook"}> <i className="fa-brands fa-facebook redes"></i> </Link>
+      </div>
+      <div className='whatsapp'>
+        <Link to={"Whatsapp"}> <i className="fa-brands fa-whatsapp redes"></i> </Link>
+        </div>
+        <div className='insta'>
+          <Link to={"Insta"}> <i className="fa-brands fa-instagram redes"></i> </Link> 
+          </div>
+          <div className='ubicacion'>
+           <Link to={"Ubicacion"}> <i className="fa-solid fa-location-dot redes"></i> </Link>
+           </div>
+    </div>
+    <div className='correoTelefono'>
        <Link to={"Correo"}> <i className="fa-solid fa-envelope"> dhara.macrame@gmail.com</i> </Link>
+         <div className='telefono'>
          <Link to={"Telefono"}> <i className="fa-solid fa-phone"> +506 83151653</i> </Link>
-     </div>
+         </div>     
+     </div>  
 </div>
+<br/>
+
+<div className='navbar'>
     <Navbar expand="lg" className="bg-body-tertiary">
-      <h1 className='nombreEmprendimiento'>Dhara Artesanal </h1>
+    <img className='logo' src='src/img/IMG_20231218_211651_662 - Dhara Macrame.jpg'></img>
       <Container>
-        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+        <Navbar.Brand href="#home">Home</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#link">Link</Nav.Link>
-            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+            <Nav.Link href="#home">Productos</Nav.Link>
+            <Nav.Link href="#link">Sobre Nosotros</Nav.Link>
+            <NavDropdown title="Seleccione una opcion" id="basic-nav-dropdown">
+              <NavDropdown.Item href="#action/3.1">Registrarse</NavDropdown.Item>
               <NavDropdown.Item href="#action/3.2">
-                Another action
+              Inicio Sesion
               </NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3">Cerrar Sesion</NavDropdown.Item>
               <NavDropdown.Divider />
               <NavDropdown.Item href="#action/3.4">
                 Separated link
@@ -46,6 +59,7 @@ function BasicExample() {
         </Navbar.Collapse>
       </Container>
     </Navbar>
+    </div>
     </>
   );
 }
