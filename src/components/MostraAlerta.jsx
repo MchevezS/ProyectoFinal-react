5aba8bcd07e700e11c7e3dce0b0bdf7ec20f7111
MchevezS <168207997+MchevezS@users.noreply.github.com>
2024-09-19 14:41:29 -0600

    sweetAlert

---
+++ src/components/MostraAlerta.jsx
@@ -0,0 +1,23 @@
+
+import Swal from 'sweetalert2'
+import '/src/index.css'
+import { Await } from 'react-router-dom'
+const Toast = Swal.mixin({
+    toast: true,
+    position: 'center',
+    iconColor: 'white',
+    customClass: {
+      popup: 'colored-toast',
+    },
+    showConfirmButton: false,
+    timer: 5000,
+    timerProgressBar: true,
+  })
+
+  const mostrarAlerta = async (icon, title)=>{
+    await Toast.fire({
+        icon: icon,
+        title: title,
+     })
+  }
+export {mostrarAlerta}
