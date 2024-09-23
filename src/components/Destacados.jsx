4f6e8fd9decc6d9cbb3feaaa35383b51f552bca9
MchevezS <168207997+MchevezS@users.noreply.github.com>
2024-09-23 15:35:42 -0600

    imagenes. productos destacados y otras secciones

--- src/components/Destacados.jsx
+++ src/components/Destacados.jsx
@@ -5,8 +5,27 @@ function Destacados() {
     <div>
       <p> Los más vendidos </p>
       <div>
-        <img></img>
+        <img className='colgante1' src='src/img/colgante de plantas.png'></img>
+       <p>Colgantes para plantas </p>
+       <p>₡6000</p>
       </div>
+     <div>
+      <img src='src/img/aretes 2.png'></img>
+      <p>Aretes</p>
+      <p>₡3000</p>
+     </div>
+     <div>
+      <img src='src/img/tapiz.png'></img>
+      <p>Tapiz grande 150cm x90cm </p>
+      <p>₡80000.</p>
+     </div>
+
+
+
+
+
+
+
     </div>
   )
 }
