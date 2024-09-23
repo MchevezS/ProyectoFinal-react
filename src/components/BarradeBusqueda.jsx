63789f4f0d19bd162211784015178e957e0ef37e
MchevezS <168207997+MchevezS@users.noreply.github.com>
2024-09-23 15:58:15 -0600

    cambios en mis productos destacados y en la barra de busqueda

---
+++ src/components/BarradeBusqueda.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function BarradeBusqueda() {
+  return (
+    <div>
+    <input type='search' placeholder='buscar....'></input>
+
+
+
+
+
+    </div>
+  )
+}
+
+export default BarradeBusqueda
--- src/components/Destacados.jsx
+++ src/components/Destacados.jsx
@@ -1,33 +1,36 @@
-import React from 'react'
+import React from 'react';
+import '../style/Destacados.css'
+import { useState } from 'react';
 
 function Destacados() {
-  return (
-    <div>
-      <p> Los más vendidos </p>
-      <div>
-        <img className='colgante1' src='src/img/colgante de plantas.png'></img>
-       <p>Colgantes para plantas </p>
-       <p>₡6000</p>
+  const [carrito, setCarrito] = useState([]);
+
+  const agregarAlCarrito = (producto) => {
+      setCarrito([...carrito, producto]);
+      // Aquí podrías agregar la lógica para guardar en localStorage si lo necesitas
+      localStorage.setItem('carrito', JSON.stringify([...carrito, producto]));
+  };
+
+  const productos = [
+      { id: 1, nombre: 'Colgantes para plantas', precio: 6000, imagen: 'src/img/colgante de plantas.png' },
+      { id: 2, nombre: 'Aretes', precio: 3000, imagen: 'src/img/aretes 2.png' },
+      { id: 3, nombre: 'Tapiz grande 150cm x90cm', precio: 80000, imagen: 'src/img/tapiz.png' },
+  ];
+    return (
+      <div className="destacados">
+      <h2>Los más vendidos</h2>
+      <div className="productos">
+          {productos.map(producto => (
+              <div key={producto.id} className="producto">
+                  <img className="imagen-producto" src={producto.imagen} alt={producto.nombre} />
+                  <p>{producto.nombre}</p>
+                  <p>₡{producto.precio}</p>
+                  <button className="btn-agregar" onClick={() => agregarAlCarrito(producto)}>Agregar al carrito</button>
+              </div>
+          ))}
       </div>
-     <div>
-      <img src='src/img/aretes 2.png'></img>
-      <p>Aretes</p>
-      <p>₡3000</p>
-     </div>
-     <div>
-      <img src='src/img/tapiz.png'></img>
-      <p>Tapiz grande 150cm x90cm </p>
-      <p>₡80000.</p>
-     </div>
-
-
-
-
-
-
-
-    </div>
-  )
+  </div>
+    );
 }
 
-export default Destacados
+export default Destacados;
